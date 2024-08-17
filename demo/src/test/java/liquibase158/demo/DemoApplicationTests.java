184aa30ca75754132574eb67d117a8d20dc46aaa
Azhar <mdazharbagwan1@gmail.com>
2024-08-17 23:19:36 +0530

    pust to git

---
+++ demo/src/test/java/liquibase158/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package liquibase158.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
